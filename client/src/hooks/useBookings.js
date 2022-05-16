50dd01051eb7ea1a2ec50340338500c8caf96aab
RJ060501 <rjrussell605@gmail.com>
2022-05-15 20:19:15 -0600

    Configuring booking under AppLayout

---
+++ client/src/hooks/useBookings.js
@@ -0,0 +1,71 @@
+import { useCallback, useEffect, useState } from "react";
+
+import {
+  httpGetBookings,
+  httpSubmitBooking,
+  httpAbortBooking,
+} from './requests';
+
+function useBookings(onSuccessSound, onAbortSound, onFailureSound) {
+  const [bookings, saveBookings] = useState([]);
+  const [isPendingBooking, setPendingBooking] = useState(false);
+
+  const getBookings = useCallback(async () => {
+    const fetchedBookings = await httpGetBookings();
+    saveBookings(fetchedBookings);
+  }, []);
+
+  useEffect(() => {
+    getBookings();
+  }, [getBookings]);
+
+  const submitBooking = useCallback(async (e) => {
+    e.preventDefault();
+    // setPendingBooking(true);
+    const data = new FormData(e.target);
+    const bookingDate = new Date(data.get("booking-day"));
+    const mission = data.get("mission-name");
+    const rider = data.get("rider-name");
+    const trail = data.get("trail-selector");
+    const response = await httpSubmitBooking({
+      bookingDate,
+      mission,
+      rider,
+      trail,
+    });
+
+    // TODO: Set success based on response.
+    const success = false;
+    if (success) {
+      getBookings();
+      setTimeout(() => {
+        setPendingBooking(false);
+        onSuccessSound();
+      }, 800);
+    } else {
+      onFailureSound();
+    }
+  }, [getLaunches, onSuccessSound, onFailureSound]);
+
+  const abortBooking = useCallback(async (id) => {
+    const response = await httpAbortBooking(id);
+
+    // TODO: Set success based on response.
+    const success = false;
+    if (success) {
+      getBookings();
+      onAbortSound();
+    } else {
+      onFailureSound();
+    }
+  }, [getBookings, onAbortSound, onFailureSound]);
+
+  return {
+    bookings,
+    isPendingBooking,
+    submitBooking,
+    abortBooking,
+  };
+}
+
+export default useBookings;
---
+++ client/src/pages/AppLayout.js
@@ -0,0 +1,100 @@
+import {
+    useState,
+  } from "react";
+  import {
+    Switch,
+    Route,
+  } from "react-router-dom";
+  import {
+    Frame,
+    withSounds,
+    withStyles,
+  } from "arwes";
+  
+  import useBookings from "../hooks/useBookings";
+  
+//   import Centered from "../components/Centered";
+//   import Header from "../components/Header";
+//   import Footer from "../components/Footer";
+  
+  import Book from "./Book";
+//   import History from "./History";
+//   import Upcoming from "./Upcoming";
+  
+  const styles = () => ({
+    content: {
+      display: "flex",
+      flexDirection: "column",
+      height: "100vh",
+      margin: "auto",
+    },
+    centered: {
+      flex: 1,
+      paddingTop: "20px",
+      paddingBottom: "10px",
+    },
+  });
+  
+  const AppLayout = props => {
+    const { sounds, classes } = props;
+  
+    const [frameVisible, setFrameVisible] = useState(true);
+    const animateFrame = () => {
+      setFrameVisible(false);
+      setTimeout(() => {
+        setFrameVisible(true);
+      }, 600);
+    };
+  
+    const onSuccessSound = () => sounds.success && sounds.success.play();
+    const onAbortSound = () => sounds.abort && sounds.abort.play();
+    const onFailureSound = () => sounds.warning && sounds.warning.play();
+  
+    const {
+      bookings,
+      isPendingBooking,
+      submitBooking,
+      abortBooking,
+    } = useBookings(onSuccessSound, onAbortSound, onFailureSound);
+    
+    return <div className={classes.content}>
+      <Header onNav={animateFrame} />
+      <Centered className={classes.centered}>
+        <Frame animate 
+          show={frameVisible} 
+          corners={4} 
+          style={{visibility: frameVisible ? "visible" : "hidden"}}>
+          {anim => (
+            <div style={{padding: "20px"}}>
+            <Switch>
+              <Route exact path="/">
+                <Book
+                  entered={anim.entered}
+                  submitBooking={submitBooking}
+                  isPendingBooking={isPendingBooking} />
+              </Route>
+              {/* <Route exact path="/launch">
+                <Launch
+                  entered={anim.entered}
+                  submitBooking={submitBooking}
+                  isPendingBooking={isPendingBooking} />
+              </Route>
+              <Route exact path="/upcoming">
+                <Upcoming
+                  entered={anim.entered}
+                  bookings={bookings}
+                  abortBooking={abortBooking} />
+              </Route>
+              <Route exact path="/history">
+                <History entered={anim.entered} bookings={bookings} />
+              </Route> */}
+            </Switch>
+            </div>
+          )}
+        </Frame>
+      </Centered>
+      <Footer />
+    </div>;
+  };
+  
+  export default withSounds()(withStyles(styles)(AppLayout));
